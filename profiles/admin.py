a6a8c6275b6b3bfb097c1c5e077456cda648c97e
Noah-Lc <noah.source@gmail.com>
2019-01-04 00:10:08 +0100

    Update admin to support custom user profile model.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+            (None, {'fields': ('email', 'password')}),
+            ( ('Personal Info'), {'fields': ('name',)}),
+            ( ('Permissions'), { 'fields': ('is_active', 'is_staff', 'is_superuser', ) }),
+            ( ('Important dates'), {'fields': ('last_login',)}), )
 
-admin.site.register(models.UserProfile)
+    add_fieldsets = (
+            (None, { 'classes': ('wide',), 'fields': ('email', 'password1', 'password2') }),
+)
+
+admin.site.register(models.UserProfile, UserAdmin)
 admin.site.register(models.ProfileFeedItem)
---
+++ profiles/tests/test_admin.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(email='admin@noah-lc.com', name='admin', password='randomPASS@123')
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(email='test@noah-lc.com', name='test', password='randomPASS@123')
+
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""
+        url = reverse('admin:profiles_userprofile_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_page_change(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:profiles_userprofile_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:profiles_userprofile_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
