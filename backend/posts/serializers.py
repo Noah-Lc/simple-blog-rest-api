301eaf1fa263874575cb4fbb813eee1f5982e9ce
Noah Lc <noah.source@gmail.com>
2020-01-06 22:46:30 +0100

    update backend flakes8 tests.

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,5 +1,6 @@
 
 import base64
+import uuid
 
 from django.core.files.base import ContentFile
 from rest_framework import serializers
@@ -26,15 +27,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class Base64ImageField(serializers.ImageField):
-    def from_native(self, data):
-        if isinstance(data, basestring) and data.startswith('data:image'):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
             # base64 encoded image - decode
-            format, imgstr = data.split(';base64,')  # format ~= data:image/X,
-            ext = format.split('/')[-1]  # guess file extension
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super(Base64ImageField, self).from_native(data)
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            id = uuid.uuid4()
+            data = ContentFile(base64.b64decode(imgstr), name = id.urn[9:] + '.' + ext)
+        return super(Base64ImageField, self).to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
